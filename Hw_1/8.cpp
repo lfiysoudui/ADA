c1a887848be188b15cb6b47b365a2622ead3a034
Donald Hsu <Donaldhsu93@gmail.com>
2023-09-25 14:26:07 +0800

    q8 1st try failed

--- Hw_1/8.cpp
+++ Hw_1/8.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-#include <vector>
+#define int long long
+
 using namespace std;
 
-int main() {
+
+
+signed main() {
     int turns ;
     cin >> turns;
     for(int k = 0; k < turns; k++){
         int length, target;
         cin >> length >> target;
-        vector<int> arr(length);
+        int arr[length];
         for(int i = 0; i < length; i++){
             cin >> arr[i];
         }
@@ -17,12 +20,18 @@ int main() {
         int l = 0, sum = 0;
         for(int r = 0; r < length; r++){
             sum += arr[r];
-            if(arr[r] > sum) l = r;
+            if(sum<0){
+                r++;
+                l = r;
+                sum = arr[r];
+            }
             while (sum >= target && r >= l){
                 // cout << "[r = " << r << " l = " << l << "]\n";
                 minlen = min( minlen, r-l+1 );
                 sum -= arr[l];
                 l++;
+                while (l>0) l--;
+                
             }
         }
         if(minlen == length+1 ) cout << "-1\n";
