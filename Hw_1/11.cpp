efe7499be629f14747dd7f02f154ff56bea3a0e4
Donald Hsu <Donaldhsu93@gmail.com>
2023-10-06 14:30:41 +0800

    11(D&C/closest point) AC

--- Hw_1/11.cpp
+++ Hw_1/11.cpp
@@ -1,5 +1,9 @@
 #include "iostream"
 #include "vector"
+#include "algorithm"
+#include "utility"
+#include "math.h"
+#include "climits"
 #define int long long
 
 using namespace std;
@@ -11,26 +15,39 @@ int findMinSub (int l, int r, vector<int>& sum){
         // cout << "1( " << l << ", " << r<< ") " << tmp << "\n";
         return tmp;
     }
-    else if (r - l < 1){
-        int tmp = (sum.front() - sum.back()) * (sum.front() - sum.back()) + size(sum) * size(sum);
-        // cout << "2( " << l << ", " << r<< ") " << tmp << "\n";
+    else if (r - l == 2){
+        int tmp = (sum[r] - sum[l+1]) * (sum[r] - sum[l+1]) + 1;
+        int tmp1 = (sum[r-1] - sum[l]) * (sum[r-1] - sum[l]) + 1;
+        int tmp2 = (sum[r] - sum[l]) * (sum[r] - sum[l]) + 4;
+        // cout << "1( " << l << ", " << r<< ") max\n";
+        tmp = min(tmp, tmp1);
+        tmp = min(tmp, tmp2);
         return tmp;
     }
+    else if (r - l < 1){
+        cout << "fail";
+        return 0;
+    }
+
     else{
         int rmin = findMinSub((l+r)/2+1, r, sum);
         int lmin = findMinSub(l, (l+r)/2, sum);
         if(rmin < lmin) swap( rmin, lmin);
         int minVal = lmin;
-        int lcur = (l+r)/2 - minVal, rcur = (l+r)/2 + minVal;
+        int lcur = (l+r)/2 - (int)sqrt(minVal) - 1, rcur = (l+r)/2 + (int)sqrt(minVal) + 1;
         if(lcur < l) lcur = l;
         if(rcur > r) rcur = r;
-        for(int i = lcur; i < rcur; i++){
-            for(int j = 1; j < 8 && j+i <= rcur; j++){
-                int tmp = (sum[i+j] - sum[i]) * (sum[i+j] - sum[i]) + j*j;
+        vector<pair<int,int>> sumsort;
+        for(int i = 0; i < rcur - lcur + 1; i++) sumsort.push_back( make_pair( sum[lcur + i],i));
+        sort(sumsort.begin(),sumsort.end());
+        // cout << "3( " << l << ", " << r<< ") bef: " << minVal << "\n";
+        for(int i = 0; i < rcur - lcur + 1; i++){
+            for(int j = 1; j < 8 && j+i < sumsort.size(); j++){
+                int tmp = (sumsort[i+j].first - sumsort[i].first) * (sumsort[i+j].first - sumsort[i].first) + (sumsort[i+j].second - sumsort[i].second) * (sumsort[i+j].second - sumsort[i].second);
                 if(tmp < minVal ) minVal = tmp;
+                // cout << "3( " << l << ", " << r<< ") in :" << sumsort[i+j]<<"(" <<i+j<< "), " << sumsort[i] << "(" <<i<< ")\n";
             }
         }
-        // cout << "3( " << l << ", " << r<< ") " << minVal << "\n";
         return minVal;
     }
 }
