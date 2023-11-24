cb27a2b37ad9a7a5097283b4606e35af1577e34c
Donald Hsu <Donaldhsu93@gmail.com>
2023-11-24 11:05:20 +0800

    12 add ref, 16 fin

--- HW_2/16.cpp
+++ HW_2/16.cpp
@@ -33,28 +33,24 @@ signed main (){
     for(int j = 0; j < n; j++){
         if(!status[0][j]){
             dp[0][j] = (j > 0)? dp[0][j-1] : 0;
-            jump[0][j] = (j > 0)? 2 : 0;
         }
         else{
             int d1 = (j >= dist0)? dp[0][j-dist0] + 1 : 1;
             int d2 = (j >= dist1)? dp[1][j-dist1] + 1 : 1;
             int d3 = (j >= dist2)? dp[2][j-dist2] + 1 : 1;
+            int d3a = (j >= dist2-1)? dp[2][j-dist2-1] + 1 : 1;
             dp[0][j] = max( d1, d2 );
             if(k == 5) {
-                if((jump[2][j-dist2] == 0)& dp[0][j] < d3){
+                dp[0][j] = max({ dp[0][j], dp[0][j-1], d3a });
+                if((jump[2][j-dist2] == 0) && dp[0][j] < d3){
                     jump[0][j] = 1;
                     dp[0][j] = d3;
                 }
-                else if((jump[2][j-dist2] == 2)& dp[0][j] < d3){
-                    dp[0][j] = d3;
-                }
-                if(dp[0][j] == dp[0][j-1]) jump[0][j] = 2;
             }
             else dp[0][j] = max( dp[0][j], d3 );
         }
         if(!status[1][j]){
             dp[1][j] = (j > 0)? dp[1][j-1] : 0;
-            jump[1][j] = (j > 0)? 2 : 0;
         }
         else{
             int d1 = (j >= dist1)? dp[0][j-dist1] + 1 : 1;
@@ -64,33 +60,30 @@ signed main (){
         }
         if(!status[2][j]){
             dp[2][j] = (j > 0)? dp[2][j-1] : 0;
-            jump[2][j] = (j > 0)? 2 : 0;
         }
         else{
             int d1 = (j >= dist2)? dp[0][j-dist2] + 1 : 1;
             int d2 = (j >= dist1)? dp[1][j-dist1] + 1 : 1;
             int d3 = (j >= dist0)? dp[2][j-dist0] + 1 : 1;
+            int d1a = (j >= dist2-1)? dp[0][j-dist2-1] + 1 : 1;
             dp[2][j] = max( d2, d3 );
             if(k == 5) {
-                if((jump[0][j-dist2] == 0)& dp[2][j] < d1){
+                dp[2][j] = max({ dp[2][j], dp[2][j-1], d1a });
+                if((jump[0][j-dist2] == 0) && dp[2][j] < d1){
                     jump[2][j] = 1;
                     dp[2][j] = d1;
                 }
-                else if((jump[0][j-dist2] == 2)& dp[2][j] < d1){
-                    dp[2][j] = d1;
-                }
-                if(dp[2][j] == dp[2][j-1]) jump[2][j] = 2;
             }
             else dp[2][j] = max( dp[2][j], d1 );
         }
     }
-    cout << max({dp[0][n-1],dp[2][n-1],dp[2][n-1]}) << "\n";
-    // for(int i = 0; i < 3; i++){
-    //     for(int j = 0; j < n; j++){
-    //         cerr << dp[i][j] << " ";
-    //     }
-    //     cerr << "\n";
-    // }
+    cout << max({dp[0][n-1],dp[1][n-1],dp[2][n-1]}) << "\n";
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < n; j++){
+            cerr << dp[i][j] << " ";
+        }
+        cerr << "\n";
+    }
     // cerr << "\n";
     // for(int i = 0; i < 3; i++){
     //     for(int j = 0; j < n; j++){
